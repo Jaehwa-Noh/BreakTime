e483a75b3e268c1df984d280b990a1763d71f674
Jedi <shwoghk14@gmail.com>
2022-04-30 21:16:10 +0900

    Initial Commit

---
+++ BreakTime/BreakTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakTimeApp.swift
+//  BreakTime
+//
+//  Created by Jaehwa Noh on 2022/04/30.
+//
+
+import SwiftUI
+
+@main
+struct BreakTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreakTime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BreakTime
+//
+//  Created by Jaehwa Noh on 2022/04/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
